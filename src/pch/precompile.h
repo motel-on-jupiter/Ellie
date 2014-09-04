2001f2e676f6b48c9c9212190ae352312b730b47
bobozeta <bobo_zeta@motel-on-jupiter.sakuraweb.com>
2014-09-05 01:10:27 +0900

    プリコンパイルヘッダからラッパー以外のインクルードを削除する

--- src/pch/precompile.h
+++ src/pch/precompile.h
@@ -1,9 +1,6 @@
 /**
  * Copyright (C) 2014 The Motel on Jupiter
  */
-#include "util/auxiliary/assert_aux.h"
-#include "util/auxiliary/csyntax_aux.h"
-#include "util/auxiliary/math_aux.h"
 #include "util/wrapper/atb_wrap.h"
 #include "util/wrapper/crtdbg_wrap.h"
 #include "util/wrapper/glgraphics_wrap.h"
